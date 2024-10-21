a9a9b2c29504ae6924267a6a5225d70b4ef2887f
hitesh-mindfire <mfsi.hiteshs@gmail.com>
2024-10-21 23:53:11 +0530

    minor changes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default async function Home() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-300 to-indigo-300 p-6">
-      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-violet-700 text-center mb-10">
+      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-violet-700 text-center mb-6">
         <img src="https://docs.saleor.io/img/logo.svg" className="w-36 h-10" />
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
